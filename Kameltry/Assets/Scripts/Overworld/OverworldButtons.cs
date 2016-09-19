1cce7ab5aa51e8572d0e4e90d0ba8740b9878a74
Conrado Adevany Clarke <conrado.clarke@cnova.com>
2016-09-19 13:49:49 -0300

    Start Menu + Overworld 0.1

---
+++ Kameltry/Assets/Scripts/Overworld/OverworldButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OverworldButtons : MonoBehaviour {
+
+    private AsyncOperation op;
+
+    public void StartLevel(string levelName)
+    {
+        if (op != null) return;
+        op = SceneManager.LoadSceneAsync(levelName);
+    }
+
+    void Update()
+    {
+        if (op == null) return;
+
+        if (op.isDone)
+        {
+            SceneManager.UnloadSceneAsync("Overworld");           
+            op = null;
+            this.enabled = false;
+        }
+    }
+}
---
+++ Kameltry/Assets/Scripts/StartMenu/StartMenuButtons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuButtons : MonoBehaviour
+{
+    private AsyncOperation op;
+    private AsyncOperation unloadOp;
+    public void StartGame()
+    {
+        if (op != null) return;
+        op = SceneManager.LoadSceneAsync("Overworld");
+    }
+
+    public void Options()
+    {
+
+    }
+
+    void Update()
+    {
+        if (op == null) return;
+
+        if (op.isDone)
+        {
+            SceneManager.UnloadSceneAsync("Title Screen");
+            op = null;
+        }
+    }
+}
